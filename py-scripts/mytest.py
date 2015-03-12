4e6a6d04f6bf14f22172ca2e22f030615171e9c1
Pronoy Barua <pronoy.barua93@gmail.com>
2015-03-12 15:24:00 +0530

    Added command line arguments

--- py-scripts/mytest.py
+++ py-scripts/mytest.py
@@ -1,16 +1,26 @@
 # Basic Full Page OCR generating HTML
-
+# usage: python mytest.py --image <Path to image>
 
 import tesseract
 import ctypes
 import os
+import argparse
+
+# parsing console arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required = True,
+	help = "Path to the image")
+args = vars(ap.parse_args())
+
+image = args["image"]
+
+# setting up the Tesseract API
 api = tesseract.TessBaseAPI()
 api.SetOutputName("outputName");
 api.Init(".","eng",tesseract.OEM_DEFAULT)
 api.SetPageSegMode(tesseract.PSM_AUTO)
-mImgFile = "testcases/multi-para.png"
 
-result = tesseract.ProcessPagesWrapper(mImgFile,api)
+result = tesseract.ProcessPagesWrapper(image,api)
 htmlResult = "<html>" + result.replace("\n"," ") + "</html>"
 fileObject = open("htmlOut.html","wb")
 fileObject.write(htmlResult)
